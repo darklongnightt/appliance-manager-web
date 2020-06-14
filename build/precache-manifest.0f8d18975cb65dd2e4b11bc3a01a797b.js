3c03dd48403fa18d2bf12ce89912639e02251430
Xavier Xu <ftt.xavier.xu@grabtaxi.com>
2020-06-14 19:42:45 +0800

    Added build to commit

---
+++ build/precache-manifest.0f8d18975cb65dd2e4b11bc3a01a797b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470c2381e61ebcb2642ca8494c586826",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d819a09c361385e0dc62",
+    "url": "/static/css/main.4b7b5451.chunk.css"
+  },
+  {
+    "revision": "2382360888e2a5a85e83",
+    "url": "/static/js/2.1780a7b8.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.1780a7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d819a09c361385e0dc62",
+    "url": "/static/js/main.1bd278da.chunk.js"
+  },
+  {
+    "revision": "dd793b8fede7de2a900c",
+    "url": "/static/js/runtime-main.4d26dd67.js"
+  },
+  {
+    "revision": "db5b4257a572fc343167fff5002e477f",
+    "url": "/static/media/logo-grey.db5b4257.svg"
+  }
+]);
